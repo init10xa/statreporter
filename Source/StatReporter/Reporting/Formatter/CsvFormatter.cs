8ce34f7d9104dbe449153c41f23074967eac851f
Mohammad Riazat <moriazat@gmail.com>
2018-06-13 15:30:44 -0400

    Adds basic content formatting capability
    Adds basic content formatting capability including a CSV formatter.
    
    Relates to: #1

---
+++ Source/StatReporter/Reporting/Formatter/CsvFormatter.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Text;
+
+namespace StatReporter.Reporting.Formatter
+{
+    public class CsvFormatter : IContentFormatter
+    {
+        private StringBuilder content;
+
+        public CsvFormatter()
+        {
+            content = new StringBuilder();
+        }
+
+        public ContentType ContentType
+        {
+            get { return ContentType.CSV; }
+        }
+
+        public string ContentFileExtension
+        {
+            get { return "txt"; }
+        }
+
+        public void AddToContent(params object[] items)
+        {
+            for (int i = 0; i < items.Length - 1; i++)
+                content.Append($"{items[i]}, ");
+
+            string lastItem = items[items.Length - 1].ToString();
+            content.AppendLine(lastItem);
+        }
+
+        public object GetFormattedContent()
+        {
+            return content.ToString();
+        }
+    }
+}
---
+++ Source/StatReporter/Reporting/Formatter/IContentFormatter.cs
@@ -0,0 +1,14 @@
+﻿
+namespace StatReporter.Reporting.Formatter
+{
+    public interface IContentFormatter
+    {
+        ContentType ContentType { get; }
+
+        string ContentFileExtension { get; }
+
+        void AddToContent(params object[] items);
+
+        object GetFormattedContent();
+    }
+}
