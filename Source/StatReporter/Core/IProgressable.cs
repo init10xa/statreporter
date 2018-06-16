88c33e7e9e106388935a2fcac7941039020e8731
Mohammad Riazat <moriazat@gmail.com>
2018-06-16 17:06:45 -0400

    Adds the progress reporting structure
    Adds the IProgressable interface which is used as the structure for
    reporting how much progress has been made in a process.
    
    Relates to: #8

---
+++ Source/StatReporter/Core/IProgressable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace StatReporter.Core
+{
+    public interface IProgressable
+    {
+        event EventHandler Completed;
+
+        event EventHandler<ProgressEventArgs> ProgressChanged;
+    }
+}
---
+++ Source/StatReporter/Core/ProgressEventArgs.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace StatReporter.Core
+{
+    public class ProgressEventArgs : EventArgs
+    {
+        public ProgressEventArgs(float progress)
+        {
+            Progress = progress;
+        }
+
+        public ProgressEventArgs(float progress, string message)
+        {
+            Progress = progress;
+            Message = message;
+        }
+
+        public string Message { get; protected set; }
+
+        public float Progress { get; protected set; }
+    }
+}
