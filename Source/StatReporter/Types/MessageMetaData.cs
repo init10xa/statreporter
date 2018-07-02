2689269484be574c980c1588fc724f5062108e0a
Mohammad Riazat <moriazat@gmail.com>
2018-07-01 21:01:48 -0400

    Overloads Equals() method in MessageMetaData class
    Relates to: #13

--- Source/StatReporter/Types/MessageMetaData.cs
+++ Source/StatReporter/Types/MessageMetaData.cs
@@ -1,11 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StatReporter.Types
 {
     public class MessageMetaData
     {
+        public User Sender { get; set; }
+
         public DateTime Timestamp { get; set; }
 
-        public User Sender { get; set; }
+        public static bool operator !=(MessageMetaData lhMessage, MessageMetaData rhMessage)
+        {
+            return lhMessage.Equals(rhMessage);
+        }
+
+        public static bool operator ==(MessageMetaData lhMessage, MessageMetaData rhMessage)
+        {
+            return !lhMessage.Equals(rhMessage);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is MessageMetaData)
+            {
+                var rhMessage = obj as MessageMetaData;
+
+                if (IsSenderEqual(rhMessage.Sender) && Timestamp.Equals(rhMessage.Timestamp))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 43549415;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Sender.Name);
+            hashCode = hashCode * -1521134295 + Timestamp.ToString("HH:mm:ss").GetHashCode();
+            return hashCode;
+        }
+
+        private bool IsSenderEqual(User sender)
+        {
+            return Sender.Name == sender.Name;
+        }
     }
 }
